c1b52159bcfef3ce284caf66b43f9895ca3f64cb
Elizabeth Lam <43484130+eli-lam@users.noreply.github.com>
2020-02-03 18:36:17 -0500

    Create templates

--- index.php
+++ index.php
@@ -1,25 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+<?php include "templates/header.php"; ?>
 
-    <title>Simple Database App</title>
+<ul>
+	<li><a href="create.php"><strong>Create</strong></a> - add a user</li>
+	<li><a href="read.php"><strong>Read</strong></a> - find a user</li>
+</ul>
 
-    <link rel="stylesheet" href="css/style.css" />
-  </head>
-
-  <body>
-    <h1>Simple Database App</h1>
-
-    <ul>
-      <li>
-        <a href="create.php"><strong>Create</strong></a> - add a user
-      </li>
-      <li>
-        <a href="read.php"><strong>Read</strong></a> - find a user
-      </li>
-    </ul>
-  </body>
-</html>
+<?php include "templates/footer.php"; ?>
